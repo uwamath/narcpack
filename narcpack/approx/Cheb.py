4ed4b9c8d75becc50f3095323c03d7929ab7776b
Tyler <chentylerl@gmail.com>
2019-02-06 10:59:38 -0800

    add basic __init__ functionality and basis functions

--- narcpack/approx/Cheb.py
+++ narcpack/approx/Cheb.py
@@ -3,18 +3,53 @@ import numpy as np
 class Cheb:
     """Very simple polynomial class
 
-    This is an extremely simple polynomial class for use in approximation classes.
+    This is an extremely simple Chebyshev polynomial class for use in approximation classes.
     """
 
-    def __init__(self, coeffs):
-        """Init Poly with the given coefficients (beginning with degree 0)"""
-        self.coeffs = np.array(coeffs)
+    def __init__(self, f, interval=[-1,1], n=10):
+        """
+        Parameters
+        ----------
+        f : scalar valued function or coefficients for basis
+        interval : array_like, optional
+               Left and right endpoints of interval over which to approximate f.
+               f is approximated over interval[0] <= x <= interval[1]
+        n : integer
+            degree of approximation
+        """
+        
+        # check if f is a callable function or list
+        if callable(f):
+            a,b = interval
+	    x = (np.arange(n+1)+0.5)*np.pi/(n+1)
+
+	    T = np.cos(np.outer(np.arange(n+1),x))
+	    
+	    c = (2/(n+1))*T.dot(f(np.cos(x))) # could implement as DCT instead of matrix multiply
+	    c[0] /= 2
+
+            self.coeffs = c
+
+        elif isinstance(f, (list, tuple, np.ndarray)):
+            self.coeffs = np.array(f)
+
+        else:
+            except ValueError:
+                print("Cheb class must be initialized on scalar function or list like object")
 
     def get_nth_basis(n):
         """
         return n-th Chebyshev polynomial
+        
+        Parameters
+        ----------
+        n : integer
+
+        Returns
+        -------
+        callable function (n-th Chebyshev polynomial)
         """
-        return None
+        return lambda x: np.cos(n*np.arccos(x))
 
     def __add__(self, other):
         """Override the + operator"""
