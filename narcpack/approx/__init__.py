67778420030f43dfea0fffed1edcded473b75025
Andreas Freund <af0@uw.edu>
2019-02-07 13:27:59 -0800

    Updated tests
    Fixed tests to work with Cheb and output more information

--- narcpack/approx/__init__.py
+++ narcpack/approx/__init__.py
@@ -1,3 +1,4 @@
 from .Poly import Poly
 from .LLS import LLS
 from .Cheb import Cheb
+from .bspline import onePoint
--- narcpack/tests/test_approx.py
+++ narcpack/tests/test_approx.py
@@ -1,36 +1,54 @@
 from unittest import TestCase
 import numpy as np
+import warnings
 
 from narcpack.approx import LLS
-classes = [LLS]
+from narcpack.approx import Cheb
+
+classes = [LLS,Cheb]
+optargs = [{}, {'n':100}]
+
+warnings.filterwarnings('ignore')
 
 class TestConstant(TestCase):
     def test_constant(self):
-        g = lambda x : 0
-        for f in classes:
-            a = f(g, [0,1])
+        g = lambda x : 0*x
+        for n, f in enumerate(classes):
+            a = f(g, [0,1], **optargs[n])
+            x = np.linspace(0,1,1000)
+            print('test_constant '+f.__name__+' '+str(np.max(np.abs(a.eval(x)-g(x)))))
 
     def test_abs(self):
         g = lambda x : np.abs(x)
-        for f in classes:
-            a = f(g, [-1,1])
+        for n, f in enumerate(classes):
+            a = f(g, [-1,1], **optargs[n])
+            x = np.linspace(-1,1,1000)
+            print('test_abs '+f.__name__+' '+str(np.max(np.abs(a.eval(x)-g(x)))))
 
     def test_step(self):
         g = lambda x : np.sign(x)
-        for f in classes:
-            a = f(g, [-1,1])
+        for n, f in enumerate(classes):
+            a = f(g, [-1,1], **optargs[n])
+            x = np.linspace(-1,1,1000)
+            print('test_step '+f.__name__+' '+str(np.max(np.abs(a.eval(x)-g(x)))))
 
     def test_runge(self):
         g = lambda x : 1.0/(1.0+x**2)
-        for f in classes:
-            a = f(g, [-1,1])
+        for n, f in enumerate(classes):
+            a = f(g, [-1,1], **optargs[n])
+            x = np.linspace(-1,1,1000)
+            print('test_runge '+f.__name__+' '+str(np.max(np.abs(a.eval(x)-g(x)))))
 
     def test_exp(self):
         g = lambda x : np.exp(x)
-        for f in classes:
-            a = f(g, [0,8])
+        for n, f in enumerate(classes):
+            a = f(g, [0,8], **optargs[n])
+            x = np.linspace(0,8,1000)
+            print('test_exp '+f.__name__+' '+str(np.max(np.abs(a.eval(x)-g(x)))))
 
     def test_sing(self):
         g = lambda x : np.exp(1.0/(1.0-x))
-        for f in classes:
-            a = f(g, [-1,1])
+        for n, f in enumerate(classes):
+            a = f(g, [-1,1], **optargs[n])
+            x = np.linspace(-1,1,1000)
+            print('test_sing '+f.__name__+' '+str(np.max(np.abs(a.eval(x)-g(x)))))
