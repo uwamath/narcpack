857ebccf66121dc9ba111b0d46c4c378921f7198
DCashon <dcashon@uw.edu>
2019-02-13 11:50:20 -0800

    further work

--- narcpack/approx/OrthoPoly.py
+++ narcpack/approx/OrthoPoly.py
@@ -1,2 +1,30 @@
 import numpy as np
 
+class OrthoPoly:
+    """
+    Orthogonal Polynomial approximation
+    on given data, interval
+
+    Implements Gram-Schmidt orthogonalization
+    to construct the polynomial basis
+
+    Parameters
+    func: function to approximate
+    interval: tuple (start, end)
+    n: number of polynomials to use to approximate
+    m: number of points to use
+
+    """
+    def __init__(self, func, interval, n, m):
+        self.func = func
+        self.interval = interval
+        self.n = n
+        self.m = m
+        self.data = func(np.linspace(interval(0), interval(-1), m))
+
+        # gram-schmidt
+        p0 = 1 # base
+        for i in range(n):
+            break
+
+# YOLO
