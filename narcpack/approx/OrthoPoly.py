4f6931485e74703479fb32dfa73be9b82e408021
DCashon <dcashon@uw.edu>
2019-02-14 11:31:15 -0800

    further work on orthogonal polynomial approximation. working sort of

--- narcpack/approx/OrthoPoly.py
+++ narcpack/approx/OrthoPoly.py
@@ -11,20 +11,44 @@ class OrthoPoly:
     Parameters
     func: function to approximate
     interval: tuple (start, end)
-    n: number of polynomials to use to approximate
-    m: number of points to use
 
     """
-    def __init__(self, func, interval, n, m):
+    def __init__(self, func, interval):
         self.func = func
         self.interval = interval
-        self.n = n
-        self.m = m
-        self.data = func(np.linspace(interval(0), interval(-1), m))
+
+    def approximate(self, n, m):
+        """
+        Gram-Schmidt to construct orthogonal polynomial basis
+        Returns the m approximated points using n degree polynomial
+
+        Parameters
+        n: number of polynomials to use as basis
+        m: number of points to evaluate the function at
+        """
+        self.x = np.linspace(self.interval[0], self.interval[-1], m)
+        self.data = self.func(self.x)
 
         # gram-schmidt
-        p0 = 1 # base
-        for i in range(n):
-            break
+        self.poly = [np.ones(m)]
+        if n > 1:
+            for i in range(1,n):
+                tempx = self.x**i
+                for j in range(len(self.poly)):
+                    tempx -= (tempx @ self.poly[j] / (self.poly[j] @ self.poly[j])) * self.poly[j]
+                self.poly.append(tempx.copy())
 
-# YOLO
+        # calculate coefficients
+        ck = []
+        ak = []
+        for polys in self.poly:
+            ck.append(np.trapz(polys**2, self.x))
+            ak.append(np.trapz(polys * self.data, self.x) / ck[-1])
+
+        # output
+        self.out = np.zeros(m)
+        for i, polys in enumerate(self.poly):
+            self.out += ak[i] * polys
+        return self.x, self.out, ak, self.poly
+        
+
