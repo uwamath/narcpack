8624557188cab9d4a443db69dbd064c1114a5ce4
Tyler <chentylerl@gmail.com>
2019-02-14 09:26:31 -0800

    update Cheb class to accept arbitrary intervals

--- narcpack/approx/Cheb.py
+++ narcpack/approx/Cheb.py
@@ -17,6 +17,8 @@ class Cheb:
         n : integer
             degree of approximation
         """
+
+        self.interval = interval
         
         # check if f is a callable function or list
         if callable(f):
@@ -24,9 +26,9 @@ class Cheb:
             
             x = (np.arange(n+1)+0.5)*np.pi/(n+1)
             
-            T = np.cos(np.outer(np.arange(n+1),x))
+            T = np.cos(np.outer(np.arange(n+1),2*(x-a)/(b-a)-1))
             
-            c = (2/(n+1))*T@f(np.cos(x)) # could implement as DCT instead of matrix multiply
+            c = (2/(n+1))*T@f(np.cos(2*(x-a)/(b-a)-1)) # could implement as DCT instead of matrix multiply
             c[0] /= 2
             
             self.coeffs = c
@@ -40,7 +42,7 @@ class Cheb:
             raise ValueError("Cheb class must be initialized on scalar function or list like object")
         
         # need j=j because of python 'late binding closures'
-        self.basis = [(lambda x, j=j: np.cos(j*np.arccos(x))) for j in range(n)]
+        self.basis = [(lambda x, j=j: np.cos(j*np.arccos(2*(x-a)/(b-a)-1))) for j in range(n)]
         
     def __add__(self, other):
         """Override the + operator"""
@@ -50,7 +52,7 @@ class Cheb:
         """Override the - operator"""
         return Cheb(self.coeffs-other.coeffs)
 
-    def eval(self, x):
+    def __call__(self, x):
         """
         A function to evaluate the polynomial at given point or points x.
         
@@ -59,6 +61,9 @@ class Cheb:
         There are better algorithms for evaluating Chebyshev polynomials
         """
         
+        # check that evaluation point(s) are in the correct interval
+        assert np.all( (self.interval[0] <= x) * (x <= self.interval[1]) ), f"x must be in the interval {self.interval}"
+
         a = 0.0
         for j in range(self.n):
             a += self.coeffs[j]*self.basis[j](x)
